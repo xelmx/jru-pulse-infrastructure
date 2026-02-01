4c0c996b1be4801a0122cab347ec22b111615416
Lyle Earl <lyle.dvcode@gmail.com>
2026-02-01 15:11:16 +0800

    feat: Security Role (Permission)- ECS Task Execution role (AWS Platform) and Task Role (App code)

---
+++ iam.tf
@@ -0,0 +1,37 @@
+# 1. ECS Task Execution Role - Allows the ECS Agent to pull images from ECR and send logs to CloudWatch
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# Standard AWS Policy for ECS Execution
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# 2. ECS Task Role - Python/PHP code uses WHILE running.
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.project_name}-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
