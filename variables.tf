78909e4ae45283eadd2334715a900bc93c7c61ce
Lyle Earl <lyle.dvcode@gmail.com>
2026-05-10 18:48:44 +0800

    modified variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,26 @@ variable "app_count" {
   type    = number
   default = 1
 }
+
+
+variable "db_password" {
+  description = "RDS Root Password"
+  type        = string
+  sensitive   = true
+}
+
+variable "google_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "google_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "mail_password" {
+  type      = string
+  sensitive = true
+}
+
